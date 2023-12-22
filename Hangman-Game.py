ebb8bbbd0b488cc48b15893d16de8e0291aff90d
Ryan Evans <139583451+RyanCodeGit@users.noreply.github.com>
2023-12-22 06:59:07 +0000

    Cleans up code to allow for imports, adds clarifying comments and simplifies play_game function's docstring. Adds spacing to conform to PEP-8

--- Hangman-Game.py
+++ Hangman-Game.py
@@ -1,5 +1,6 @@
 import random as ra
 
+
 class Hangman:
     """
     This class is designed to allow a user to play hangman against their computer.
@@ -14,15 +15,15 @@ class Hangman:
     Args:
         word_list (list) = A list of words for the class to pick from for the user to guess.
         num_lives (int) = The number of lives the user has before the game ends. Defaults to 5.
-
     """
+
     def __init__(self, word_list, num_lives=5):
         """
         See help(Hangman) for explanations of the attributes defined here.
         """
-        self.word = ra.choice(word_list).lower()
-        self.word_guessed = ['_' for character in self.word]
-        self.num_letters = len(set(self.word))
+        self.word = ra.choice(word_list).lower() # Conversion to lower-case for consistency in _check_guess().
+        self.word_guessed = ['_' for character in self.word] # Visual representation of the word the user is guessing.
+        self.num_letters = len(set(self.word)) # Number of unique letters left in self.word.
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
@@ -38,10 +39,10 @@ class Hangman:
         Args:
             guess (str) = A single alphabetic character, designed to be passed from the ask_for_input() method.
         """
-        guess = guess.lower()
+        guess = guess.lower() # Conversion to lower-case for checking against self.word.
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
-            for index, character in enumerate(self.word):
+            for index, character in enumerate(self.word): # Adds correctly guessed letter to visual representation.
                 if character == guess:
                     self.word_guessed[index] = guess
             self.num_letters -= 1
@@ -67,20 +68,20 @@ class Hangman:
                 self._check_guess(guess)
                 self.list_of_guesses.append(guess)
                 break
-                
+
 
 def play_game(word_list):
     """
-    This function will start a game of hangman using the Hangman class defined in this file. 
+    This function will start a game of hangman using the Hangman class defined in this file.
     
-    This function creates an instance of the Hangman class, then loops until the user is finished playing. The user will be told how many letters are in the word they are guessing,
-    then shown a representation of the word with non-guessed letters hidden by underscores, a list of letters they have already guessed and the amount of lives they have remaining.
-    As long as there are letters left in the word, the user will be asked to input guesses for the remaining letters. Once there are no letters or lives remaining, the game is over.
-    When the game is over, the user will be asked if they would like to play again. If they input "y", this function will be called again. If they input anything else, the loop breaks.
+    This function creates an instance of the Hangman class, then loops until the game is over.
+    Once the game is over, the function will recur via the rematch section if the user responds "Y".
+    The user will be given information showing how many letters remain, which letters they've
+    already guessed and their remaining lives before they make each guess.
 
     Attributes:
-        num_lives (int) = The number of lives that will be passed to the Hangman class to start the game with. If you would like more or less lives, change the value.
-        game (Hangman) = An instance of class Hangman that passes the word_list argument of this function and the num_lives attribute as arguments for the instance. 
+        num_lives (int) = The number of lives that will be passed to the Hangman class to start the game with.
+        game (Hangman) = An instance of the Hangman class with this function's word_list and num_lives passed as initialising attributes. 
                          See help(Hangman) for more detail on the class.
     
     Args:
@@ -96,7 +97,7 @@ def play_game(word_list):
             print(f"No more lives remaining. The word was {game.word}.")
             break
         elif game.num_letters > 0:
-            print("~~~~~~~~~~")
+            print("~~~~~~~~~~")  # Included to make the game easier to read while playing.
             print(f"Current letters: {game.word_guessed}")
             print(f"Letters you've guessed: {game.list_of_guesses}")
             print(f"Lives: {game.num_lives}")
@@ -105,18 +106,18 @@ def play_game(word_list):
             print(game.word_guessed)
             print(f"Congratulations, the word was {game.word}. You win!")
             break
-    rematch = str(input("Play again? (Y or N): ")).upper()
+    
+    # Code to allow user to play again after game over.
+    rematch = str(input("Play again? (Y or N): ")).upper() # Conversion to upper-case to simplify following condition.
     if rematch == "Y":
         play_game(word_list)
     else:
         print("Thank you for playing!")
 
-# Help files. Un-comment these lines and comment out the play_game(wl) call below if you would like to view help documentation.
-# help(play_game)
-# help(Hangman)
 
-# Default word list
-wl = ["Grape", "Pineapple", "Mango", "Strawberry", "Banana"]
+if __name__ == "__main__":
+    # Default word list.
+    wl = ["Grape", "Pineapple", "Mango", "Strawberry", "Banana"]
 
-# This line starts the game. If you would like to use a custom word_list, please replace the argument "wl" with a list of words of your choosing.
-play_game(wl)
+    # This line starts the game. If you would like to use a custom word_list, please replace the argument "wl" with a list of words of your choosing.
+    play_game(wl)
